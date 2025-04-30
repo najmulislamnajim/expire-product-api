727bbe252ce1ba98ef79264e359546a36425470c
najmulislamnajim <ninbd17@gmail.com>
2025-04-30 12:25:32 +0600

    add material list view

--- material_app/views.py
+++ material_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from material_app.models import RplMaterial
+from material_app.serializers import RplMaterialSerializer
 
 # Create your views here.
+class RplMaterialListView(APIView):
+    def get(self, request):
+        materials = RplMaterial.objects.all()
+        serializer = RplMaterialSerializer(materials, many=True)
+        return Response({"success": True, "data": serializer.data}, status=status.HTTP_200_OK)
