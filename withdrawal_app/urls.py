e6e89fb7f261495e6169441f3ff7c02b3fc64bb2
najmulislamnajim <ninbd17@gmail.com>
2025-05-04 14:27:58 +0600

    add request approval url

--- withdrawal_app/urls.py
+++ withdrawal_app/urls.py
@@ -4,4 +4,5 @@ from .import views as withdrawal_views
 urlpatterns = [
     path('request', withdrawal_views.WithdrawalRequestView.as_view(), name='withdrawal_request'),
     path('list', withdrawal_views.WithdrawalListView.as_view(), name='withdrawal_list'),
+    path('request/<str:invoice_no>/approve', withdrawal_views.RequestApproveView.as_view(), name='request_approve'),
 ]
