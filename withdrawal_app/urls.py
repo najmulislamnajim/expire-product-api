665e0eb0857308dec5e7a009092d2dc9bf0d85d0
najmulislamnajim <ninbd17@gmail.com>
2025-05-04 11:16:34 +0600

    add request url

--- withdrawal_app/urls.py
+++ withdrawal_app/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .import views as withdrawal_views
 
 urlpatterns = [
+    path('request', withdrawal_views.WithdrawalRequestView.as_view(), name='withdrawal_request'),
 ]
