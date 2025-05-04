41f0dc71960a72558ef6b423208a0c4b8cbbe603
najmulislamnajim <ninbd17@gmail.com>
2025-05-04 12:52:10 +0600

    add list url

--- withdrawal_app/urls.py
+++ withdrawal_app/urls.py
@@ -3,4 +3,5 @@ from .import views as withdrawal_views
 
 urlpatterns = [
     path('request', withdrawal_views.WithdrawalRequestView.as_view(), name='withdrawal_request'),
+    path('list', withdrawal_views.WithdrawalListView.as_view(), name='withdrawal_list'),
 ]
