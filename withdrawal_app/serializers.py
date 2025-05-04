bfabf402c0d3e32ef687662be3e9fa073c55864a
najmulislamnajim <ninbd17@gmail.com>
2025-05-04 11:58:19 +0600

    restructure fields

--- withdrawal_app/serializers.py
+++ withdrawal_app/serializers.py
@@ -32,10 +32,41 @@ class WithdrawalRequestSerializer(serializers.ModelSerializer):
     
     This serializer is used to create a withdrawal request.
     """
+    # These fields are both readable and writable
+    mio_id = serializers.IntegerField()
+    rm_id = serializers.IntegerField()
+    da_id = serializers.IntegerField()
+    depot_id = serializers.IntegerField()
+    route_id = serializers.IntegerField()
+    partner_id = serializers.IntegerField()
+
+    # Read-only fields
+    request_date = serializers.DateField(read_only=True)
+    request_approval = serializers.BooleanField(read_only=True)
+    withdrawal_confirmation = serializers.BooleanField(read_only=True)
+    replacement_order = serializers.BooleanField(read_only=True)
+    order_approval = serializers.BooleanField(read_only=True)
+    order_delivery = serializers.BooleanField(read_only=True)
+    last_status = serializers.CharField(read_only=True)
+    request_date = serializers.DateField(read_only=True)
+    request_approval_date = serializers.DateField(read_only=True)
+    withdrawal_date = serializers.DateField(read_only=True)
+    withdrawal_approval_date = serializers.DateField(read_only=True)
+    order_date = serializers.DateField(read_only=True)
+    order_approval_date = serializers.DateField(read_only=True)
+    delivery_date = serializers.DateField(read_only=True)
+    last_status = serializers.CharField(read_only=True)
+    # request list (withdrawal requested products)
     request_list = WithdrawalRequestListSerializer(many=True)
     class Meta:
         model = WithdrawalInfo
-        fields = '__all__'
+        fields = [
+            'mio_id', 'rm_id', 'da_id', 'depot_id', 'route_id', 'partner_id',
+            'request_approval', 'withdrawal_confirmation', 'replacement_order',
+            'order_approval', 'order_delivery', 'last_status', 'request_date',
+            'request_approval_date', 'withdrawal_date', 'withdrawal_approval_date',
+            'order_date', 'order_approval_date', 'delivery_date', 'last_status', 'request_list'
+        ]
     
     def create(self, validated_data):
         """
