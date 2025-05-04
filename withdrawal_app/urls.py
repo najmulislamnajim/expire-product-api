56c0abb3170c22f32b26fe0e916e42e34d9c7568
najmulislamnajim <ninbd17@gmail.com>
2025-05-04 15:25:36 +0600

    add withdrawal confirmation url

--- withdrawal_app/urls.py
+++ withdrawal_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('request', withdrawal_views.WithdrawalRequestView.as_view(), name='withdrawal_request'),
     path('list', withdrawal_views.WithdrawalListView.as_view(), name='withdrawal_list'),
     path('request/<str:invoice_no>/approve', withdrawal_views.RequestApproveView.as_view(), name='request_approve'),
+    path('<str:invoice_no>/confirm', withdrawal_views.WithdrawalConfirmationView.as_view(), name='withdrawal_confirmation'),
 ]
