abb5caec23f49d2513c08ac50729913aba792d2b
najmulislamnajim <ninbd17@gmail.com>
2025-05-04 11:16:21 +0600

    add withdrawal request view

--- withdrawal_app/views.py
+++ withdrawal_app/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+import logging
+from datetime import date
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from withdrawal_app.serializers import WithdrawalRequestSerializer
 
+# Set logger
+logger = logging.getLogger("withdrawal_app")
 # Create your views here.
+class WithdrawalRequestView(APIView):
+    """
+    View to create a new withdrawal request.
+    
+    Args:
+        request (Request): The HTTP request object.
+    
+    Returns:
+        Response: A response object containing the created withdrawal request data.
+    """
+    def post(self, request):
+        """
+        Create a new withdrawal request.
+        
+        Args:
+            request (Request): The HTTP request object.
+        
+        Returns:
+            Response: A response object containing the created withdrawal request data.
+        """ 
+        data = request.data.copy()
+        # Set default values
+        data['request_approval'] = False
+        data['withdrawal_confirmation'] = False
+        data['replacement_order'] = False
+        data['order_approval'] = False
+        data['order_delivery'] = False
+        data['last_status'] = 'request'
+        data['request_date'] = date.today()
+        mio = data.pop('mio_id')
+        # Validate and save
+        serializer = WithdrawalRequestSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            logger.info("Withdrawal request created successfully for MIO %s", mio)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        logger.error(f"Error creating withdrawal request {mio} : {serializer.errors}")
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
