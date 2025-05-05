be7e5da1819449d92d36a40f0233dabefcc539f7
najmulislamnajim <ninbd17@gmail.com>
2025-05-05 14:53:54 +0600

    add withdrawal list

--- withdrawal_app/views.py
+++ withdrawal_app/views.py
@@ -7,6 +7,7 @@ from drf_spectacular.utils import extend_schema
 from django.shortcuts import get_object_or_404
 from withdrawal_app.serializers import WithdrawalRequestSerializer, WithdrawalSerializer, WithdrawalListSerializer, DaAssignSerializer
 from withdrawal_app.models import WithdrawalInfo
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiResponse
 
 # Set logger
 logger = logging.getLogger("withdrawal_app")
@@ -96,6 +97,8 @@ class WithdrawalRequestListView(APIView):
             queryset = queryset.filter(request_approval=False)
         elif stat == 'request_approved':
             queryset = queryset.filter(request_approval=True)
+        elif stat == 'all':
+            queryset = queryset
         else:
             return Response({"detail": "Please provide a valid status."}, status=status.HTTP_404_NOT_FOUND)           
 
@@ -130,33 +133,13 @@ class RequestApproveView(APIView):
         withdrawal_request.request_approval_date = date.today()
         withdrawal_request.save()
         return Response({"detail": "Withdrawal request approved successfully."}, status=status.HTTP_200_OK)
-    
-class WithdrawalConfirmationView(APIView):
-    """
-    View to confirm a withdrawal request.
-    """
-    def put(self, request, invoice_no):
-        """
-        Confirm a withdrawal request.
-        
-        Args:
-            request (Request): The HTTP request object.
-            invoice_no (str): The invoice no of the withdrawal request to be confirmed.
-        
-        Returns:
-            Response: A response object containing the confirmation status.
-        """
-        withdrawal_request = get_object_or_404(WithdrawalInfo, invoice_no=invoice_no)
-        withdrawal_request.withdrawal_confirmation = True
-        withdrawal_request.withdrawal_approval_date = date.today()
-        withdrawal_request.save()
-        return Response({"detail": "Withdrawal request confirmed successfully."}, status=status.HTTP_200_OK)
-    
+       
     
 class DaAssignView(APIView):
     """
     View to assign a delivery agent to a withdrawal request.
     """
+    serializer_class = DaAssignSerializer
     @extend_schema(request=DaAssignSerializer)
     def put(self, request, invoice_no):
         """
@@ -214,3 +197,102 @@ class WithdrawalSaveView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         logger.error(f"Error creating withdrawal {da_id} : {serializer.errors}")
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class WithdrawalListView(APIView):
+    """
+    Handles GET requests for retrieving withdrawal list based on the parameters.
+    
+    Filters the data based on `mio_id`, `rm_id`, `depot_id`, `da_id`, and `status`.
+    status should be: all, withdrawal_list, withdrawal_approved, order_pending, order_approved, order_delivered
+    """
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(name='mio_id', description='Mio ID', required=False, type=str),
+            OpenApiParameter(name='rm_id', description='RM ID', required=False, type=str),
+            OpenApiParameter(name='depot_id', description='Depot ID', required=False, type=str),
+            OpenApiParameter(name='da_id', description='Delivery Agent ID', required=False, type=str),
+            OpenApiParameter(name='status', description='Filter status of the withdrawal request', required=True, type=str, enum=['all', 'withdrawal_list', 'withdrawal_approved', 'order_pending', 'order_approved', 'order_delivered'])
+        ],
+        responses={
+            status.HTTP_200_OK: WithdrawalSerializer(many=True),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(description="Invalid request parameters"),
+            status.HTTP_404_NOT_FOUND: OpenApiResponse(description="No matching records found"),
+        }
+    )
+    def get(self, request):
+        """
+        Handles GET requests for retrieving withdrawal list based on the parameters.
+    
+        Filters the data based on `mio_id`, `rm_id`, `depot_id`, `da_id`, and `status`.
+        status should be: all, withdrawal_list, withdrawal_approved, order_pending, order_approved, order_delivered
+        """
+        mio_id = request.query_params.get('mio_id')
+        rm_id = request.query_params.get('rm_id')
+        depot_id = request.query_params.get('depot_id')
+        da_id = request.query_params.get('da_id')
+        stat = request.query_params.get('status')  # request_pending, request_approved, withdrawal_list, withdrawal_approved, order_pending, order_approved, order_delivered
+
+        queryset = WithdrawalInfo.objects.all()  # Default query
+        serializer_class = WithdrawalSerializer # for  schema generation tools
+         
+        if not any([mio_id, rm_id, depot_id, da_id]):
+            return Response({"detail": "Please provide at least one ID (mio_id, rm_id, depot_id, or da_id)."}, status=status.HTTP_400_BAD_REQUEST)
+        if not stat:
+            return Response({"detail": "Please provide a valid status."}, status=status.HTTP_404_NOT_FOUND)
+
+        # Filtering based on provided parameters
+        if mio_id:
+            queryset = queryset.filter(mio_id=mio_id)
+        if rm_id:
+            queryset = queryset.filter(rm_id=rm_id)
+        if depot_id:
+            queryset = queryset.filter(depot_id=depot_id)
+        if da_id:
+            queryset = queryset.filter(da_id=da_id)
+        
+        # Filter based on the status parameter
+        if stat == 'withdrawal_list':
+            queryset = queryset.filter(request_approval=True, withdrawal_confirmation=False)
+        elif stat == 'withdrawal_approved':
+            queryset = queryset.filter(withdrawal_confirmation=True)
+        elif stat == 'order_pending':
+            queryset = queryset.filter(withdrawal_confirmation=True,order_approval=False)
+        elif stat == 'order_approved':
+            queryset = queryset.filter(order_approval=True)
+        elif stat == 'order_delivered':
+            queryset = queryset.filter(order_delivery=True)
+        elif stat == 'all':
+            queryset = queryset 
+        else:
+            return Response({"detail": "Please provide a valid status."}, status=status.HTTP_404_NOT_FOUND)           
+
+        # Ensure we return a meaningful response
+        if not queryset.exists():
+            logger.error(f"No matching records found. {mio_id}, {rm_id}, {depot_id}, {da_id}, {status}")
+            return Response({"detail": "No matching records found."}, status=status.HTTP_404_NOT_FOUND)
+        
+        # Serialize the queryset and return it as a response
+        serializer = WithdrawalSerializer(queryset, many=True)
+        logger.info(f"Approval list fetched successfully for {mio_id}, {rm_id}, {depot_id}, {da_id}, {status}")
+        return Response(serializer.data, status=status.HTTP_200_OK)    
+    
+class WithdrawalConfirmationView(APIView):
+    """
+    View to confirm a withdrawal request.
+    """
+    def put(self, request, invoice_no):
+        """
+        Confirm a withdrawal request.
+        
+        Args:
+            request (Request): The HTTP request object.
+            invoice_no (str): The invoice no of the withdrawal request to be confirmed.
+        
+        Returns:
+            Response: A response object containing the confirmation status.
+        """
+        withdrawal_request = get_object_or_404(WithdrawalInfo, invoice_no=invoice_no)
+        withdrawal_request.withdrawal_confirmation = True
+        withdrawal_request.withdrawal_approval_date = date.today()
+        withdrawal_request.save()
+        return Response({"detail": "Withdrawal request confirmed successfully."}, status=status.HTTP_200_OK)
