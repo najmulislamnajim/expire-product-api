67b1a1e15ed71043d1d56ca90bf965b7d7796f7a
najmulislamnajim <ninbd17@gmail.com>
2025-05-05 12:40:36 +0600

    add save url, modify endpoints

--- withdrawal_app/urls.py
+++ withdrawal_app/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .import views as withdrawal_views
 
 urlpatterns = [
-    path('request', withdrawal_views.WithdrawalRequestView.as_view(), name='withdrawal_request'),
     path('list', withdrawal_views.WithdrawalListView.as_view(), name='withdrawal_list'),
-    path('request/<str:invoice_no>/approve', withdrawal_views.RequestApproveView.as_view(), name='request_approve'),
-    path('<str:invoice_no>/confirm', withdrawal_views.WithdrawalConfirmationView.as_view(), name='withdrawal_confirmation'),
-    path('<str:invoice_no>/assign', withdrawal_views.DaAssignView.as_view(), name='da_assign'),
+    path('request', withdrawal_views.WithdrawalRequestView.as_view(), name='withdrawal_request'),
+    path('request/approve/<str:invoice_no>', withdrawal_views.RequestApproveView.as_view(), name='request_approve'),
+    path('assign-da/<str:invoice_no>', withdrawal_views.DaAssignView.as_view(), name='da_assign'),
+    path('save/<str:invoice_no>',withdrawal_views.WithdrawalSaveView.as_view(), name='withdrawal_save'),
+    path('confirm/<str:invoice_no>', withdrawal_views.WithdrawalConfirmationView.as_view(), name='withdrawal_confirmation'),
 ]
