bcedc76ce4bf388d51990e4ba3393aea742c3e76
najmulislamnajim <ninbd17@gmail.com>
2025-05-29 12:04:20 +0600

    remove invoice no from url

--- withdrawal_app/urls.py
+++ withdrawal_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('request', withdrawal_views.WithdrawalRequestView.as_view(), name='withdrawal_request'),
     path('request/edit', withdrawal_views.WithdrawalRequestUpdateView.as_view(), name='withdrawal_request_edit'),
     path('request/approve/<str:invoice_no>', withdrawal_views.RequestApproveView.as_view(), name='request_approve'),
-    path('assign-da/<str:invoice_no>', withdrawal_views.DaAssignView.as_view(), name='da_assign'),
+    path('assign-da', withdrawal_views.DaAssignView.as_view(), name='da_assign'),
     path('save/<str:invoice_no>',withdrawal_views.WithdrawalSaveView.as_view(), name='withdrawal_save'),
     path('confirm/<str:invoice_no>', withdrawal_views.WithdrawalConfirmationView.as_view(), name='withdrawal_confirmation'),
 ]
