fd6799f56137b0eef9049306c202ec6a7da48223
najmulislamnajim <ninbd17@gmail.com>
2025-08-07 13:56:15 +0600

    add pagination settings

--- expire_product_api/settings.py
+++ expire_product_api/settings.py
@@ -147,6 +147,8 @@ runserver.default_port = "5001"
 # API documentation setup
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 # Documentation settings
