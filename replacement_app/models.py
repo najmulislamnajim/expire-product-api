70850a2adeef5e444c3bb1124dde435a0f95e2b7
najmulislamnajim <ninbd17@gmail.com>
2025-08-11 15:21:17 +0600

    add replacement model and migrations

--- replacement_app/models.py
+++ replacement_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from withdrawal_app.models import WithdrawalInfo
 
-# Create your models here.
+# Create your models here.            
+class ReplacementList(models.Model):
+    invoice = models.ForeignKey(WithdrawalInfo, on_delete=models.CASCADE, related_name='replacement_list')
+    matnr = models.CharField(max_length=40)
+    batch = models.CharField(max_length=40, null=True, blank=True)
+    pack_qty = models.IntegerField(default=0)
+    unit_qty = models.IntegerField(default=0) # TAB/CAP/AMP
+    net_val = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.invoice} replacement list'
+    
+    class Meta:
+        db_table = 'expr_replacement_list'
+        verbose_name = 'Replacement List'
+        verbose_name_plural = 'Replacement List'
